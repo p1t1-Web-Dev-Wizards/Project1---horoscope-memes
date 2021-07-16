ef3cab62da5f041a655cb0b6d29ee39b766aee74
sylv4030 <77740560+sylv4030@users.noreply.github.com>
2021-07-16 17:15:32 -0500

    menu script and formatting

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
    var elems = document.querySelectorAll('select');
    var instances = M.FormSelect.init(elems, {});
+
+   console.log(elems);
  });
+
+ var inputForm = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var dayInput = document.querySelector('#day-input').value;
+  var signInput = document.querySelector('#sign-input').value;
+
+  console.log(dayInput);
+  console.log(signInput);
+}
+
+inputForm.addEventListener('submit', handleSearchFormSubmit);
